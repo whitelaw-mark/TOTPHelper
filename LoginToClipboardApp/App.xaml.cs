c3764e9f90ded518ffc880a484efeaec9cf62538
whitelaw-mark <mark.whitelaw@triad.co.uk>
2026-05-21 16:53:43 +0100

    merge mistakes

--- LoginToClipboardApp/App.xaml.cs
+++ LoginToClipboardApp/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using LoginClipboardApp.Model;
+using LoginClipboardApp.ViewModel;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.IO;
 using System.Windows;
 
 namespace LoginClipboardApp
@@ -9,16 +13,38 @@ namespace LoginClipboardApp
     /// </summary>
     public partial class App : Application
     {
-        private Window? view;
-        private BaseViewModel? viewModel;
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        public IConfiguration Configuration { get; private set; }
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
-            viewModel = new AppViewModel() ;
-            view = new MainWindow();
-            view.DataContext = viewModel;
-            view.ShowDialog();
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // make configuration available if other services need it
+            services.AddSingleton(Configuration);
+
+            // bind the "Profiles" section to a List<UserCredentialsProfile> and register it with the options system
+            //services.AddOptions();
+            //services.Configure<List<UserCredentialsProfile>>(Configuration.GetSection("Profiles"));
+
+            services.AddSingleton<AppViewModel>();
+            services.AddTransient(typeof(MainWindow));
         }
     }
 }
---
+++ LoginToClipboardApp/Model/UserCredentialsProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace LoginClipboardApp.Model
+{
+    public class UserCredentialsProfile
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string totpSecret { get; set; }  
+    }
+
+    public class UserCredentialsProfileCollection
+    {
+        public UserCredentialsProfile[] Profiles { get; set; }
+    }
+}
